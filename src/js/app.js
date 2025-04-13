5678737ea3004ab6219a914209740c0245e4f82c
Galaceon <antoniogarcialtr@gmail.com>
2025-04-14 01:52:32 +0200

    project finished  1.0

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     navegacionFija()
     crearGaleria()
+    resaltarEnlace()
+    scrollNav()
 })
 
 function navegacionFija() {
@@ -67,4 +69,42 @@ function cerrarModal() {
         const body = document.querySelector('body');
     body.classList.remove('overflow-hidden');
     }, 500)
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id;
+            }
+        })
+
+        navLinks.forEach( link => {
+            link.classList.remove('active');
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active');
+            }
+        })
+    })
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
 }
