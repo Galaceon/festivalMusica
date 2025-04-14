bcc9c248239a73be3a781dddea0c5916f1707556
Galaceon <antoniogarcialtr@gmail.com>
2025-04-14 02:03:23 +0200

    gulpfile build added to netlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
-import {src, dest, watch, series} from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import { src, dest, watch, series } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
 
-export function css (done) {
-    src('src/scss/app.scss', {sourcemaps: true})
-        .pipe( sass().on('error', sass.logError))
-        .pipe( dest('build/css', {sourcemaps: true}))
+export function css(done) {
+  src('src/scss/app.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('build/css', { sourcemaps: true }));
 
-    done()
+  done();
 }
 
-export function js (done) {
-    src('src/js/app.js')
-        .pipe(dest('build/js'))
+export function js(done) {
+  src('src/js/app.js')
+    .pipe(dest('build/js'));
 
-    done();
+  done();
 }
 
 export function dev() {
-    watch('src/scss/**/*.scss', css)
-    watch('src/js/**/*.js', js)
+  watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', js);
 }
 
-export default series(js, css, dev)
+export const build = series(js, css); //build para github y netlify
+
+export default series(js, css, dev);
