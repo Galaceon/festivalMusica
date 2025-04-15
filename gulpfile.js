565430b6156f7d84936494a638d6473ef2f033ab
Galaceon <antoniogarcialtr@gmail.com>
2025-04-15 03:27:12 +0200

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import terser from 'gulp-terser';
 
 const sass = gulpSass(dartSass);
 
@@ -12,15 +14,17 @@ export function html(done) {
   }
 
 export function css(done) {
-  src('src/scss/app.scss', { sourcemaps: true })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('build/css', { sourcemaps: true }));
+    src('src/scss/app.scss', { sourcemaps: true })
+      .pipe(sass().on('error', sass.logError))
+      .pipe(cleanCSS())
+      .pipe(dest('build/css', { sourcemaps: '.' }));
 
-  done();
+    done();
 }
 
 export function js(done) {
   src('src/js/app.js')
+    .pipe(terser())
     .pipe(dest('build/js'));
 
   done();
@@ -30,7 +34,5 @@ export function dev() {
   watch('src/scss/**/*.scss', css);
   watch('src/js/**/*.js', js);
 }
-
-export const build = series(js, css, html); //build para github y netlify
 
 export default series(js, css, dev);
